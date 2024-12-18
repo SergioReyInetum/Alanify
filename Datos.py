a531fc888dc1e550e3029abefca9107d0e0517be
Sergio Rey <sergio.rey.ext@inetum.com>
2024-12-18 16:53:02 +0100

    Subo lo que llevo de los Datos.csv aunque he de cambiar varias cosas más, subo también el Logo, aunque terminaré moviendo las imagenes en una carpeta a parte y subo el codigo que conecta con la IA y genera las respuestas

---
+++ Datos.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
+# Nombre del modelo en Hugging Face
+modelo_nombre = "SergioInetum/autotrain-zogdm-5tsc6"
+
+def cargar_modelo():
+    print("Cargando el modelo y el tokenizador desde Hugging Face...")
+    tokenizer = AutoTokenizer.from_pretrained(modelo_nombre)
+    modelo = AutoModelForSequenceClassification.from_pretrained(modelo_nombre)
+    return pipeline("text-classification", model=modelo, tokenizer=tokenizer)
+
+def clasificar_frase(clasificador, frase):
+    print(f"Clasificando la frase: '{frase}'")
+    resultado = clasificador(frase)
+    return resultado
+
+if __name__ == "__main__":
+    # Cargar el modelo y el tokenizador
+    clasificador = cargar_modelo()
+
+    # Ejemplo de frases a clasificar
+    frases = [
+        "Quiero un ordenador para jugar a videojuegos exigentes.",
+        "Necesito un PC para estudiar ingeniería.",
+        "Solo quiero un ordenador para ver videos y navegar en casa.",
+        "Busco una computadora para diseñar objetos 3D.",
+        "Quiero un ordenador de oficina",
+        "Quiero un ordenador para jugar Counter-Strike",
+        "Quiero un ordenador para estar en casa y hacer algun programita en el cual también pueda jugar a juegos casuales",
+        "Ordenador para hacer videos",
+        "Quiero un ordenador par ajugar al solitario y la Candy Crash",
+        "Quiero un ordenador para jugar fortnite, ver videos y hablar con amigos por discord",
+        "Quiero jugar al vrchat",
+        "Quiero un ordenador para el niño"
+    ]
+
+    # Clasificar cada frase
+    for frase in frases:
+        resultado = clasificar_frase(clasificador, frase)
+        print("Resultado:", resultado)
