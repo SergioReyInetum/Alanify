a8f06c68849b3000ec4985fd37d77f39e04c7135
Sergio Rey <sergio.rey.ext@inetum.com>
2024-12-21 19:53:58 +0100

    Subo el archivo Mueble.js ya que no se porque no se modificó en el último Commit

--- Mueble.js
+++ Mueble.js
@@ -8,30 +8,57 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
   try {
     const data = JSON.parse(jsonString);
 
-    // Verifica si el campo "procesadores" es un arreglo
+    // Verifica si el campo "procesadores" es un array
     if (Array.isArray(data.procesadores)) {
-      // Extraer los datos de los procesadores
+      // Crear todas las variables necesarias
+      let Nombre = "";
       let Fabricante = "";
+      let Socket = "";
+
+
+      // Extraer los datos de los procesadores
       const procesadores = data.procesadores.map(procesador => ({
         nombre: procesador.nombre,
         precio: procesador.precio,
-        url: procesador.url
+        url: procesador.url,
+        caracteristicas: procesador.caracteristicas
       }));
 
-      // Imprimir los nombres de los procesadores en consola
+      // Imprimir los nombres y fabricantes de los procesadores en consola
       procesadores.forEach((procesador, index) => {
-        for (let i = 0; i < procesador.nombre.length; i++) {
-
+        Nombre = procesador.caracteristicas[0]
+        // Mirar de forma ordenada si el procesador es Intel, AMD u Otro
+        Fabricante = "";
+        if (procesador.nombre.split(/\s+/).includes("Intel")) {
+          Fabricante = "Intel";
+      }
+      
+        else if (procesador.nombre.split(/\s+/).includes("AMD") || procesador.nombre.split(/\s+/).includes("Ryzen")) {
+          Fabricante = "AMD";
+        }
+        else{
+          for (let i = 0; i < procesador.nombre.length; i++) {
             if (procesador.nombre[i] == " ") {
-                console.log(Fabricante);
-                Fabricante = "";
                 break;                
             }
-
-            Fabricante = Fabricante + procesador.nombre[i];
-            
+          Fabricante = Fabricante + procesador.nombre[i];
+          }
         }
+
+        // Mirar el socket del procesador
+        for (let i = 0; i < procesador.caracteristicas.length; i++) {
+          if (procesador.caracteristicas[i].includes("Socket") && !procesador.caracteristicas[i].includes("**Características**")) {
+              Socket = procesador.caracteristicas[i].replace(new RegExp(`\\b${"Socket"}\\b`, "g"), "").replace(new RegExp(`${":"}`), "").trim();
+              break;
+          }
+        }
+        
+        console.log(Fabricante);
+        console.log(Nombre);
+        console.log(Socket);
+        console.log();
       });
+
     } else {
       console.error('El archivo JSON no contiene un arreglo "procesadores".');
     }
