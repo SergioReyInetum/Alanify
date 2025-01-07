cc781df7b4769ad02574902321fc2dfef8e2c1b5
Sergio Rey <sergio.rey.ext@inetum.com>
2025-01-07 12:55:01 +0100

    Subo los cambios del día Martes 07/01/2025. Casi termino la parte de procesadores

--- Mueble.js
+++ Mueble.js
@@ -15,8 +15,16 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
       var Nombre = "";
       var Fabricante = "";
       var Socket = "";
-      var Gen = "";
       var Nomenclatura = "";
+      var Gen = "";
+      var Nucleos = "";
+      var NucleosEficiencia = "";
+      var NucleosRendimiento = "";
+      var Hilos = "";
+      var FrecuenciaBaseEficiencia = "";
+      var FrecuenciaBaseRendimiento = "";
+      var FrecuenciaTurboEficiencia = "";
+      var FrecuenciaTurboRendimiento = "";
 
       // Extraer los datos de los procesadores
       const procesadores = data.procesadores.map(procesador => ({
@@ -50,15 +58,6 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
                 break;                
             }
           Fabricante = Fabricante + procesador.nombre[i];
-          }
-        }
-
-        // Mirar el socket del procesador
-        Socket = "Null";
-        for (let i = 0; i < procesador.caracteristicas.length; i++) {
-          if (procesador.caracteristicas[i].includes("Socket") && !procesador.caracteristicas[i].includes("**Características**")) {
-              Socket = procesador.caracteristicas[i].replace(new RegExp(`\\b${"Socket"}\\b`, "g"), "").replace(new RegExp(`${":"}`), "").trim();
-              break;
           }
         }
 
@@ -108,10 +107,17 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
 
         // Mirar la generación de los procesadores
         Gen = "Null";
-        if(Fabricante == "AMD"){
-          for (let i = 0; i < Nombre.length; i++) {
-            if(Nombre[i] == " " && !isNaN(Nombre[i-1])){
-              switch (Nombre[i+1]) {
+        if(Fabricante == "Intel" && Nomenclatura[0] == "i"){
+          if (Nomenclatura[1] != 1) {
+            Gen = Nomenclatura[0] + Nomenclatura[1];
+          } else{
+            Gen = Nomenclatura[0] + Nomenclatura[1] + Nomenclatura[2];
+          }
+        }
+        else if(Fabricante == "AMD"){
+          for (let i = 0; i < Nomenclatura.length; i++) {
+            if(Nomenclatura[i] == " " && !isNaN(Nomenclatura[i-1])){
+              switch (Nomenclatura[i+1]) {
                 case "1":
                   Gen = "Zen";
                   break;
@@ -139,12 +145,83 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
             
           }
         }
+
+        Socket = "Null";
+        Nucleos = "Null";
+        NucleosRendimiento = "Null";
+        NucleosEficiencia = "Null";
+        Hilos = "Null";
+        FrecuenciaBaseEficiencia = "Null";
+        FrecuenciaBaseRendimiento = "Null";
+        FrecuenciaTurboEficiencia = "Null";
+        FrecuenciaTurboRendimiento = "Null";
+        for (let i = 0; i < procesador.caracteristicas.length; i++) {
+          let linea = procesador.caracteristicas[i];
+          // Mirar el socket del procesador
+          if (procesador.caracteristicas[i].includes("Socket") && !procesador.caracteristicas[i].includes("**Características**")) {
+            Socket = procesador.caracteristicas[i].replace(new RegExp(`\\b${"Socket"}\\b`, "g"), "").replace(new RegExp(`${":"}`), "").trim();
+          }
+
+          // Mirar los Nucleos e Hilos del procesador
+          if ((procesador.caracteristicas[i].includes("núcleos")||procesador.caracteristicas[i].includes("CPU")||procesador.caracteristicas[i].includes("Núcleos")) && !procesador.caracteristicas[i].includes("**Características**")) {
+            Nucleos = procesador.caracteristicas[i].replace(new RegExp(`\\b(?:Cantidad de núcleos|Número de núcleos|Núcleos CPU)\\b`, "g"), "").replace(new RegExp(`${":"}`), "").trim();
+            if(!isNaN(Nucleos[1])){
+              Nucleos = Nucleos[0] + Nucleos[1];
+            } else{
+              Nucleos = Nucleos[0];
+            }
+          }
+          // Mirar en los nucleos sus nucleos destinados a Eficiencia y Rendimiento
+          if(Nucleos != "Null" && (procesador.caracteristicas[i].includes("Rendimiento +") && !procesador.caracteristicas[i].includes("**Características**"))){
+            if(!isNaN(linea[26])){
+              NucleosRendimiento = linea[25]+linea[26];
+            } else{
+              NucleosRendimiento = linea[25];
+            }
+            if(!isNaN(linea[44])){
+              NucleosEficiencia = linea[43]+linea[44];
+            } else{
+              NucleosEficiencia = linea[43];
+            }
+          }
+          if ((procesador.caracteristicas[i].includes("hilos")||procesador.caracteristicas[i].includes("subprocesos")||procesador.caracteristicas[i].includes("Hilos")) && !procesador.caracteristicas[i].includes("**Características**")) {
+            Hilos = procesador.caracteristicas[i].replace(new RegExp(`\\b(?:Cantidad de hilos|Número de hilos|Cantidad de subprocesos|N.° de subprocesos)\\b`, "g"), "").replace(new RegExp(`${":"}`), "").trim();
+            if(!isNaN(Hilos[1])){
+              Hilos = Hilos[0] + Hilos[1];
+            } else{
+              Hilos = Hilos[0];
+            }
+          }
+          
+          // Mirar la Frecuencia base y la Frecuencia turbo
+          if (procesador.caracteristicas[i].includes("Frecuencia base") && !procesador.caracteristicas[i].includes("**Características**")) {
+            FrecuenciaBaseEficiencia = linea[44]+linea[45]+linea[46];
+            FrecuenciaBaseRendimiento = linea[53]+linea[54]+linea[55];
+          }
+          if (procesador.caracteristicas[i].includes("Frecuencia turbo") && !procesador.caracteristicas[i].includes("**Características**")) {
+            FrecuenciaTurboEficiencia = linea[45]+linea[46]+linea[47];
+            FrecuenciaTurboRendimiento = linea[55]+linea[56]+linea[57];
+          }
+
+        }        
         
         console.log("Fabricante: " + Fabricante);
         console.log("Nombre: " + Nombre);
         console.log("Nomenclatura: " + Nomenclatura);
         console.log("Socket: " + Socket);
         console.log("Gen: " + Gen);
+        console.log("Nucleos: " + Nucleos);
+        if (NucleosRendimiento != "Null"){
+          console.log("Nucleos Rendimiento: " + NucleosRendimiento);
+          console.log("Nucleos Eficiencia: " + NucleosEficiencia);
+        }
+        console.log("Hilos: " + Hilos);
+        if(FrecuenciaBaseEficiencia != "Null"){
+          console.log("Frecuencia Base Eficiencia: " + FrecuenciaBaseEficiencia);
+          console.log("Frecuencia Base Rendimiento: " + FrecuenciaBaseRendimiento);
+          console.log("Frecuencia Turbo Eficiencia: " + FrecuenciaTurboEficiencia);
+          console.log("Frecuencia Turbo Rendimiento: " + FrecuenciaTurboRendimiento);
+        }
         console.log();
       });
 
